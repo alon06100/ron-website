5a6139ca61e93612d06976f72f4b6803f65c5e0c
Alon <alon.maor@fiverr.com>
2020-02-12 00:01:23 +0200

    aaa

--- index.php
+++ index.php
@@ -39,4 +39,16 @@ switch ($request) {
     case '/eilat' :
         require 'eilat.html';
         break;
+    case '/haavoda' :
+        require 'haavoda.html';
+        break;
+    case '/neviot' :
+        require 'neviot.html';
+        break;
+    case '/achva' :
+        require 'achva.html';
+        break;
+    case '/familysoap' :
+        require 'familysoap.html';
+        break;
 }
