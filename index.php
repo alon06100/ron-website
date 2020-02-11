863d41d283c8195ae8879bb3c15afc5792ca9c7a
Alon <alon.maor@fiverr.com>
2020-02-11 23:20:11 +0200

    ddd

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include_once("homesite3.html");?>
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . 'homesite.html';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
