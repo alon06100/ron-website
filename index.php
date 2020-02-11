59343d47b49f476d6c2c550829fd7ad148527f26
Alon <alon.maor@fiverr.com>
2020-02-11 23:10:11 +0200

    test

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
 <?php include_once("homesite.html");?>
-<?php include_once("homesite2.html");?>
-<?php include_once("homesite3.html");?>
-<?php include_once("homesite4.html");?>
-<?php include_once("homesite5.html");?>
